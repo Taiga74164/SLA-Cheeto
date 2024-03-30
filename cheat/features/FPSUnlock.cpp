2adab8fd5b6c0efbcbbed560823dea7d0bb4a4e5
EtoShinya <shinyacloudcubes@gmail.com>
2024-03-30 14:10:28 +0300

    feat: Improve FPSUnlock

--- cheat/features/FPSUnlock.cpp
+++ cheat/features/FPSUnlock.cpp
@@ -5,11 +5,22 @@
 
 namespace Cheat::Features
 {
+    static bool _lastEnabledStatus = false;
+    static int _originFPS = 30;
+    
     void FPSUnlock::Update()
     {
+        if (_lastEnabledStatus && !vars.b_FPSUnlock)
+        {
+            app::Application_set_targetFrameRate(_originFPS, nullptr);
+        }
+        else if (!_lastEnabledStatus && vars.b_FPSUnlock)
+        {
+            _originFPS = app::Application_get_targetFrameRate(nullptr);
+        }
+        _lastEnabledStatus = vars.b_FPSUnlock;
         if (vars.b_FPSUnlock)
         {
-            // LOG("fps: %d", app::Application_get_targetFrameRate(nullptr));
             app::Application_set_targetFrameRate(vars.i_FPS, nullptr);
             app::QualitySettings_set_vSyncCount(0, nullptr);
         }
