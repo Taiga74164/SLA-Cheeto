bff095d8700a42e050739fbc2b3184dfcad2f069
Joaquin <67109235+Taiga74164@users.noreply.github.com>
2024-04-04 06:23:35 -0600

    fix: Email Login not showing

--- src/game/game-utils.hpp
+++ src/game/game-utils.hpp
@@ -18,8 +18,10 @@ inline bool IsInLoginScene()
 {
 	auto scene = app::SceneManager_GetActiveScene(nullptr);
 	auto sceneName = il2cppi_to_string(app::Scene_GetNameInternal(scene.m_Handle, nullptr));
-	LOG("Scene name: %s", sceneName.c_str());
-	if (sceneName == "LoginScene") return true;
+	// LOG("Scene name: %s", sceneName.c_str());
+	if (sceneName == "SceneIntro" ||
+		sceneName == "SceneLogin")
+		return true;
 
 	return false;
 }
