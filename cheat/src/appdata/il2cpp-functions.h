02ef81976fef07f2faf9c2313cd12ce4df870880
EtoShinya <shinyacloudcubes@gmail.com>
2024-11-01 15:44:25 +0300

    feat: Update 1.1.86

--- cheat/src/appdata/il2cpp-functions.h
+++ cheat/src/appdata/il2cpp-functions.h
@@ -6,61 +6,61 @@
 // ******************************************************************************
 using namespace app;
 
-DO_APP_FUNC(0x00792E70, void, GameFrameWork_Update, (void/*GameFrameWork*/* __this, MethodInfo* method));
+DO_APP_FUNC(0x0078A9F0, void, GameFrameWork_Update, (void/*GameFrameWork*/* __this, MethodInfo* method));
 
-DO_APP_FUNC(0x02CB3240, String*, Marshal_PtrToStringAnsi, (void* ptr, MethodInfo* method));
-DO_APP_FUNC(0x02CB3260, String*, Marshal_PtrToStringUni, (void* ptr, MethodInfo* method));
+DO_APP_FUNC(0x02D03D50, String*, Marshal_PtrToStringAnsi, (void* ptr, MethodInfo* method));
+DO_APP_FUNC(0x02D03D70, String*, Marshal_PtrToStringUni, (void* ptr, MethodInfo* method));
 
 // FovChanger
-DO_APP_FUNC(0x03D62F20, void, Camera_set_fieldOfView, (void* __this, float value, MethodInfo* method));
+DO_APP_FUNC(0x03D79A40, void, Camera_set_fieldOfView, (void* __this, float value, MethodInfo* method));
 
 // C# methods
-DO_APP_FUNC(0x02DECBE0, Type*, Type_GetType_3, (String* typeName, MethodInfo* method));
-DO_APP_FUNC(0x02CC71E0, Type*, Assembly_GetType, (Assembly* __this, String* name, MethodInfo* method));
-DO_APP_FUNC(0x02CC7700, Assembly*, Assembly_LoadWithPartialName, (String* partialName, MethodInfo* method));
+DO_APP_FUNC(0x02E3BC40, Type*, Type_GetType_3, (String* typeName, MethodInfo* method));
+DO_APP_FUNC(0x02D17CF0, Type*, Assembly_GetType, (Assembly* __this, String* name, MethodInfo* method));
+DO_APP_FUNC(0x02D18210, Assembly*, Assembly_LoadWithPartialName, (String* partialName, MethodInfo* method));
 // Unity methods
-DO_APP_FUNC(0x03D5BDB0, int32_t, Application_get_targetFrameRate, (MethodInfo* method));
-DO_APP_FUNC(0x03D5C4D0, void, Application_set_targetFrameRate, (int32_t value, MethodInfo* method));
-DO_APP_FUNC(0x03DBB7B0, Vector3, Vector3_Lerp, (Vector3 a, Vector3 b, float t, MethodInfo* method));
-DO_APP_FUNC(0x03DBB550, float, Vector3_Distance, (Vector3 a, Vector3 b, MethodInfo* method));
-DO_APP_FUNC(0x03DA5C00, Transform*, GameObject_get_transform, (GameObject* __this, MethodInfo* method));
-DO_APP_FUNC(0x03DA4BB0, GameObject*, GameObject_Find, (String* name, MethodInfo* method));
-DO_APP_FUNC(0x03DAA860, String*, Object_1_get_name, (Object_1* __this, MethodInfo* method));
-DO_APP_FUNC(0x03DB3510, void, Time_4_set_timeScale, (float value, MethodInfo* method));
-DO_APP_FUNC(0x03DB7840, Vector3, Transform_get_position, (Transform* __this, MethodInfo* method));
-DO_APP_FUNC(0x03DB8420, void, Transform_set_position, (Transform* __this, Vector3 value, MethodInfo* method));
-DO_APP_FUNC(0x03DB7890, Vector3, Transform_get_right, (Transform* __this, MethodInfo* method));
-DO_APP_FUNC(0x03DB8470, void, Transform_set_right, (Transform* __this, Vector3 value, MethodInfo* method));
-DO_APP_FUNC(0x03DB7B60, Vector3, Transform_get_up, (Transform* __this, MethodInfo* method));
-DO_APP_FUNC(0x03DB85F0, void, Transform_set_up, (Transform* __this, Vector3 value, MethodInfo* method));
-DO_APP_FUNC(0x03DB70B0, Vector3, Transform_get_forward, (Transform* __this, MethodInfo* method));
-DO_APP_FUNC(0x03DB7F40, void, Transform_set_forward, (Transform* __this, Vector3 value, MethodInfo* method));
-DO_APP_FUNC(0x03DB6F50, int32_t, Transform_get_childCount, (Transform* __this, MethodInfo* method));
-DO_APP_FUNC(0x03DD1330, String*, Scene_GetNameInternal, (int32_t sceneHandle, MethodInfo* method));
-DO_APP_FUNC(0x03DCFD00, Scene, SceneManager_GetActiveScene, (MethodInfo* method));
+DO_APP_FUNC(0x03D728D0, int32_t, Application_get_targetFrameRate, (MethodInfo* method));
+DO_APP_FUNC(0x03D72FF0, void, Application_set_targetFrameRate, (int32_t value, MethodInfo* method));
+DO_APP_FUNC(0x03DD3640, Vector3, Vector3_Lerp, (Vector3 a, Vector3 b, float t, MethodInfo* method));
+DO_APP_FUNC(0x03DD33E0, float, Vector3_Distance, (Vector3 a, Vector3 b, MethodInfo* method));
+DO_APP_FUNC(0x03DBDA90, Transform*, GameObject_get_transform, (GameObject* __this, MethodInfo* method));
+DO_APP_FUNC(0x03DBCA40, GameObject*, GameObject_Find, (String* name, MethodInfo* method));
+DO_APP_FUNC(0x03DC26F0, String*, Object_1_get_name, (Object_1* __this, MethodInfo* method));
+DO_APP_FUNC(0x03DCB3A0, void, Time_4_set_timeScale, (float value, MethodInfo* method));
+DO_APP_FUNC(0x03DCF6D0, Vector3, Transform_get_position, (Transform* __this, MethodInfo* method));
+DO_APP_FUNC(0x03DD02B0, void, Transform_set_position, (Transform* __this, Vector3 value, MethodInfo* method));
+DO_APP_FUNC(0x03DCF720, Vector3, Transform_get_right, (Transform* __this, MethodInfo* method));
+DO_APP_FUNC(0x03DD0300, void, Transform_set_right, (Transform* __this, Vector3 value, MethodInfo* method));
+DO_APP_FUNC(0x03DCF9F0, Vector3, Transform_get_up, (Transform* __this, MethodInfo* method));
+DO_APP_FUNC(0x03DD0480, void, Transform_set_up, (Transform* __this, Vector3 value, MethodInfo* method));
+DO_APP_FUNC(0x03DCEF40, Vector3, Transform_get_forward, (Transform* __this, MethodInfo* method));
+DO_APP_FUNC(0x03DCFDD0, void, Transform_set_forward, (Transform* __this, Vector3 value, MethodInfo* method));
+DO_APP_FUNC(0x03DCEDE0, int32_t, Transform_get_childCount, (Transform* __this, MethodInfo* method));
+DO_APP_FUNC(0x03DE9190, String*, Scene_GetNameInternal, (int32_t sceneHandle, MethodInfo* method));
+DO_APP_FUNC(0x03DE7B60, Scene, SceneManager_GetActiveScene, (MethodInfo* method));
 
 // No Cooldown
-DO_APP_FUNC(0x00B1F8B0, void/*KAAIFMKPKAG*/*, KAAIFMKPKAG_IOFMGMJCCFO, (KAAIFMKPKAG* __this, bool DEJNILEHENL, MethodInfo* method));
+DO_APP_FUNC(0x00B7F560, void/*KAAIFMKPKAG*/*, KAAIFMKPKAG_IOFMGMJCCFO, (KAAIFMKPKAG* __this, bool DEJNILEHENL, MethodInfo* method));
 // Damage Hack
-DO_APP_FUNC(0x00AEFA40, void, PIPHNBOBFEF_KBCIIEFLPGB, (PIPHNBOBFEF* __this, ESpecialState__Enum OCKAHFDGNEL, int64_t MGFPJFANLBG, int64_t DIACKALMMKH, int64_t PBAPBJGDAPJ, String* DLOACPLHGMP, MethodInfo* method));
+DO_APP_FUNC(0x00B57A80, void, PIPHNBOBFEF_KBCIIEFLPGB, (PIPHNBOBFEF* __this, ESpecialState__Enum OCKAHFDGNEL, int64_t MGFPJFANLBG, int64_t DIACKALMMKH, int64_t PBAPBJGDAPJ, String* DLOACPLHGMP, MethodInfo* method));
 // God Mode
-DO_APP_FUNC(0x00E5F7C0, int32_t, GHINOEFFMPN_EKHGIHBHEPL, (SkillIdentity* EKPKPIJBPKE, void/*IEnumerable_1_UnityEngine_Component_*/* FKJDKGJBGOD, TargetHitData* COFCDKGCOGJ, MethodInfo* method));
+DO_APP_FUNC(0x00EC4010, int32_t, GHINOEFFMPN_EKHGIHBHEPL, (SkillIdentity* EKPKPIJBPKE, void/*IEnumerable_1_UnityEngine_Component_*/* FKJDKGJBGOD, TargetHitData* COFCDKGCOGJ, MethodInfo* method));
 // Dumb Enemies
-DO_APP_FUNC(0x00EEB930, void, EvadeProxy_Init, (EvadeProxy* __this, ENNEJEPMJLJ* IGBKKNODEGM, EvadePenetration* DHPMEMDBDMC, MethodInfo* method));
+DO_APP_FUNC(0x00F5A530, void, EvadeProxy_Init, (EvadeProxy* __this, ENNEJEPMJLJ* IGBKKNODEGM, EvadePenetration* DHPMEMDBDMC, MethodInfo* method));
 
 // EntityManager
-DO_APP_FUNC(0x00B23470, void, ENHPKNIEKMI_MIFDFEHDDDD, (ENHPKNIEKMI* __this, MethodInfo* method));
-DO_APP_FUNC(0x007BD540, ENHPKNIEKMI*, ENNEJEPMJLJ_FIGHFBNIDJA, (ENNEJEPMJLJ* __this, MethodInfo* method));
-DO_APP_FUNC(0x00EA5C70, bool, PCILGJOEPJM_PPAKPBOJLIP, (ENNEJEPMJLJ* IGBKKNODEGM, MethodInfo* method)); // Check if Entity is alive????
+DO_APP_FUNC(0x00B93070, void, ENHPKNIEKMI_MIFDFEHDDDD, (ENHPKNIEKMI* __this, MethodInfo* method));
+DO_APP_FUNC(0x005E5FF0, ENHPKNIEKMI*, ENNEJEPMJLJ_FIGHFBNIDJA, (ENNEJEPMJLJ* __this, MethodInfo* method));
+DO_APP_FUNC(0x00F1BDA0, bool, PCILGJOEPJM_PPAKPBOJLIP, (ENNEJEPMJLJ* IGBKKNODEGM, MethodInfo* method)); // Check if Entity is alive????
 
 // Mission Time
-DO_APP_FUNC(0x00662780, void, StageReadyPage_EnterUI, (StageReadyPage* __this, MethodInfo* method));
-DO_APP_FUNC(0x00666260, void, StoryQuestChapterPage_EnterUI, (StoryQuestChapterPage* __this, MethodInfo* method));
+DO_APP_FUNC(0x006D21C0, void, StageReadyPage_EnterUI, (StageReadyPage* __this, MethodInfo* method));
+DO_APP_FUNC(0x006D5360, void, StoryQuestChapterPage_EnterUI, (StoryQuestChapterPage* __this, MethodInfo* method));
 // Intro Movie
-DO_APP_FUNC(0x00FD8AD0, void, IntroMovie_Update, (IntroMovie* __this, MethodInfo* method));
+DO_APP_FUNC(0x0105EBC0, void, IntroMovie_Update, (IntroMovie* __this, MethodInfo* method));
 // Disable Web View
-DO_APP_FUNC(0x02F9D320, void, WebViewDialog_Show, (void* __this, MethodInfo* method));
-DO_APP_FUNC(0x02FB1370, void, WebViewUI_RefreshSize, (void* __this, MethodInfo* method));
+DO_APP_FUNC(0x02FEA990, void, WebViewDialog_Show, (void* __this, MethodInfo* method));
+DO_APP_FUNC(0x02FFE9E0, void, WebViewUI_RefreshSize, (void* __this, MethodInfo* method));
 
 //DO_APP_FUNC(0x00430040, EGLGAPIAANF__Enum, JIBDAHKJHOG_PCLINKFFGNE, (JIBDAHKJHOG* __this, MethodInfo* method));
 //DO_APP_FUNC(0x00837FE0, void, JIBDAHKJHOG_KPDGECAFJKA, (JIBDAHKJHOG* __this, MethodInfo* method));
@@ -69,10 +69,10 @@ DO_APP_FUNC(0x02FB1370, void, WebViewUI_RefreshSize, (void* __this, MethodInfo* 
 //DO_APP_FUNC(0x00836410, void, JIBDAHKJHOG_COLDHPFDOHG, (JIBDAHKJHOG* __this, MethodInfo* method)); // StageStart
 
 // Shadow Cooldown And Limit
-DO_APP_FUNC(0x00EBEF60, void, JLLMICINIDA_LFCGJLAFFOM, (JLLMICINIDA* __this, MethodInfo* method));
+DO_APP_FUNC(0x00F25AC0, void, JLLMICINIDA_LFCGJLAFFOM, (JLLMICINIDA* __this, MethodInfo* method));
 
 // Singletons
 // DO_APP_FUNC(0x00384E60, void*, TSingleton_GetInstance, (MethodInfo* method)); // Search for this in IDA. Seems to be Singleton<T> class
-DO_APP_FUNC(0x0047DAE0, Object*, TSingleton_1_System_Object__get_Instance, (MethodInfo* method)); // Get the instance of the singleton
+DO_APP_FUNC(0x00479AE0, Object*, TSingleton_1_System_Object__get_Instance, (MethodInfo* method)); // Get the instance of the singleton
 // DO_APP_FUNC_METHODINFO(0x04038380, TSingleton_1_JIBDAHKJHOG__get_Instance__MethodInfo);
 // DO_APP_FUNC_METHODINFO(0x0403AC70, TSingleton_1_PJKMAOEINNH__get_Instance__MethodInfo); // Probably EntityManager or something similar
