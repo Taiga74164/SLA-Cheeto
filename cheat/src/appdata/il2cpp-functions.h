c5dcdde0724ddaf0d068ccbbedbc40994cd43aaf
toriello <rneox@live.com>
2024-12-02 11:00:08 -0300

    Update il2cpp-functions.h

--- cheat/src/appdata/il2cpp-functions.h
+++ cheat/src/appdata/il2cpp-functions.h
@@ -1,9 +1,6 @@
-// Generated C++ file by Il2CppInspector - http://www.djkaty.com - https://github.com/djkaty
-// Target Unity version: 2021.2.0+
-
-// ******************************************************************************
+// ***************************************************
 // * IL2CPP application-specific method definition addresses and signatures
-// ******************************************************************************
+// *********************************
 using namespace app;
 
 DO_APP_FUNC(0x00770520, void, GameFrameWork_Update, (void/*GameFrameWork*/* __this, MethodInfo* method));
